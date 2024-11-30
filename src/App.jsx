5a783725bc368a1496c59c457fa2ab1d7869ff12
Pininita <pininita92001@gmail.com>
2024-11-29 21:00:31 -0500

    fixing file css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './styles/App.css';
 import { Header } from './components/Header'
 import { Presentation } from './components/Presentation';
 import { AboutMe } from './components/AboutMe';
